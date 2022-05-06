78333d7c37e9d3f7fc55a59a233019d2486d2db0
Xaver Pinero <xaverpinero@yahoo.com>
2022-05-06 10:24:35 -0400

    First commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send("<h2>It is working!</h2>");
+});
+
+app.listen(PORT, () => {
+  console.log(`API is listening on port ${PORT}`);
+});
