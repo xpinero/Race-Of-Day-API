d508b4b5339b017012b47f8b649e796087a93ff7
Xaver Pinero <xaverpinero@yahoo.com>
2022-05-06 14:28:40 -0400

    folder set ups

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const v1Router = require("./v1/routes/");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send("<h2>It is working!</h2>");
-});
+// app.get('/', (req, res) => {
+//   res.send("<h2>It is working!</h2>");
+// });
+app.use("/api/v1", v1Router);
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
