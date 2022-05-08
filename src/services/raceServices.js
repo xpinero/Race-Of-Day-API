8c98240e64c1ed74897129b885489fad38fc30f1
Candace Pinero <xaverpinero@yahoo.com>
2022-05-07 22:55:35 -0400

    created services file

---
+++ src/services/raceServices.js
@@ -0,0 +1,27 @@
+const getAllRaces = () => {
+  return;
+};
+
+const getOneRace = () => {
+  return;
+};
+
+const createNewRace = () => {
+  return;
+};
+
+const updateOneRace = () => {
+  return;
+};
+
+const deleteOneRace = () => {
+  return;
+};
+
+module.exports = {
+  getAllRaces,
+  getOneRace,
+  createNewRace,
+  updateOneRace,
+  deleteOneRace
+};
