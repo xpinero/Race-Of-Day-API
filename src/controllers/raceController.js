240367b62601693d505e260d561fe78174d4c873
Xaver Pinero <xaverpinero@yahoo.com>
2022-05-09 13:51:17 -0400

    completed controller methods

--- src/controllers/raceController.js
+++ src/controllers/raceController.js
@@ -2,12 +2,18 @@ const raceService = require("../services/raceServices");
 
 const getAllRaces = (req, res) => {
   const allRaces = raceService.getAllRaces();
-  res.send({status: "A-OK", data: allRaces});
+  res.send({status: "OK", data: allRaces});
 };
 
 const getOneRace = (req, res) => {
-  const race = raceService.getOneRace();
-  res.send("Get an existing race");
+  const {
+    params: { raceId },
+  } = req;
+  if (!raceId) {
+    return;
+  }
+  const race = raceService.getOneRace(raceId);
+  res.send({ status: "OK", data: race });
 };
 
 const createNewRace = (req, res) => {
@@ -29,17 +35,30 @@ const createNewRace = (req, res) => {
     trainerTips: body.trainerTips,
   }
   const createdRace = raceService.createNewRace(newRace);
-  res.status(201).send({ status: "A-OK", data: createdRace });
+  res.status(201).send({ status: "OK", data: createdRace });
 };
 
 const updateOneRace = (req, res) => {
-  const updatedRace = raceService.updateOneRace();
-  res.send("Update an existing race");
+  const {
+    body,
+    params: { raceId },
+  } = req;
+  if (!raceId) {
+    return;
+  }
+  const updatedRace = raceService.updateOneRace(raceId, body);
+  res.send({ status: "OK", data: updatedRace });
 };
 
 const deleteOneRace = (req, res) => {
-  raceService.deleteOneRace();
-  res.send("Delete an existing race");
+  const {
+    params: { raceId },
+  } = req;
+  if (!raceId) {
+    return;
+  }
+  raceService.deleteOneRace(raceId);
+  res.status(204).send({ status: "OK" });
 };
 
 module.exports = {
