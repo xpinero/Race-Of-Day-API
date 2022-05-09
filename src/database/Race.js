3bdefd0b552b16009526e68e42131587b8f1b6f9
Xaver Pinero <xaverpinero@yahoo.com>
2022-05-09 14:23:32 -0400

    completed Race methods for the DB

--- src/database/Race.js
+++ src/database/Race.js
@@ -6,6 +6,14 @@ const getAllRaces = () => {
   return DB.races;
 };
 
+const getOneRace = (raceId) => {
+  const race = DB.races.find((race) => race.id === raceId);
+  if (!race) {
+    return;
+  }
+  return race;
+};
+
 const createNewRace = (newRace) => {
   const isAlreadyAdded =
     DB.races.findIndex((race) => race.name === newRace.name) > -1;
@@ -17,7 +25,38 @@ const createNewRace = (newRace) => {
   return newRace;
 };
 
+const updateOneRace = (raceId, changes) => {
+  const indexForUpdate = DB.races.findIndex(
+    (race) => race.id === raceId
+  );
+  if (indexForUpdate === -1) {
+    return;
+  }
+  const updatedRace = {
+    ...DB.races[indexForUpdate],
+    ...changes,
+    updatedAt: new Date().toLocaleString("en-US", { timeZone: "UTC"}),
+  };
+  DB.races[indexForUpdate] = updatedRace;
+  saveToDatabase(DB);
+  return updatedRace;
+};
+
+const deleteOneRace = (raceId) => {
+  const indexForDeletion = DB.races.findIndex(
+    (race) => race.id === raceId 
+  );
+  if (indexForDeletion === -1) {
+    return;
+  }
+  DB.races.splice(indexForDeletion, 1);
+  saveToDatabase(DB);
+};
+
 module.exports = { 
   getAllRaces,
+  getOneRace,
   createNewRace,
+  updateOneRace,
+  deleteOneRace
 };
