b9af1cde8376e1531017d4a5e38b5ee457de2937
CCSE ADMIN <xaverpinero@yahoo.com>
2022-05-10 09:04:25 -0400

    cleaned up some comments

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 const express = require("express");
-//const v1Router = require("./v1/routes/");
 const bodyParser = require("body-parser");
 const v1RaceRouter = require("./v1/routes/raceRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// app.get('/', (req, res) => {
-//   res.send("<h2>It is working!</h2>");
-// });
-//app.use("/api/v1", v1Router);
 app.use(bodyParser.json());
 app.use("/api/v1/races", v1RaceRouter);
 
--- src/v1/routes/raceRoutes.js
+++ src/v1/routes/raceRoutes.js
@@ -13,23 +13,3 @@ router.patch("/:raceId", raceController.updateOneRace);
 router.delete("/:raceId", raceController.deleteOneRace);
 
 module.exports = router;
-// router.get("/", (req, res) => {
-//   res.send("Get all races");
-// });
-
-// router.get("/:raceId", (req, res) => {
-//   res.send("Get an existing race");
-// });
-
-// router.post("/", (req, res) => {
-//   res.send("Create a new race");
-// });
-
-// router.patch("/:raceId", (req, res) => {
-//   res.send("Update an existing race");
-// });
-
-// router.delete("/:raceId", (req, res) => {
-//   res.send("Delete an existing race");
-// });
-
