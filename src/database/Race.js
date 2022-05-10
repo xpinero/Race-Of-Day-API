b1b3a01b6a5f7b43310f747647d60f2f79a49c43
CCSE ADMIN <xaverpinero@yahoo.com>
2022-05-10 08:52:10 -0400

    added error handling for Race data

--- src/database/Race.js
+++ src/database/Race.js
@@ -1,64 +1,96 @@
 const DB = require("./db.json");
-
 const { saveToDatabase } = require("./utils");
 
 const getAllRaces = () => {
-  return DB.races;
+  try {
+    return DB.races;
+  } catch (error) {
+    throw { status: 500, message: error };
+  }
 };
 
 const getOneRace = (raceId) => {
-  const race = DB.races.find((race) => race.id === raceId);
+  try {
+    const race = DB.races.find((race) => race.id === raceId);
   if (!race) {
-    return;
+    throw {
+      status: 400,
+      message: `Can not find race with the ID '${raceId}'`,
+    };
   }
   return race;
+  } catch (error) {
+    throw { status: error?.status || 500, message: error?.message || error };
+  }
 };
 
 const createNewRace = (newRace) => {
-  const isAlreadyAdded =
-    DB.races.findIndex((race) => race.name === newRace.name) > -1;
-  if (isAlreadyAdded) {
-    throw {
-      status: 400,
-      message: `Race with the name '${newRace.name}' already exists`,
-    };
-  }
   try {
+    const isAlreadyAdded =
+      DB.races.findIndex((race) => race.name === newRace.name) > -1;
+    if (isAlreadyAdded) {
+      throw {
+        status: 400,
+        message: `Race with the name '${newRace.name}' already exists`,
+      };
+    }
     DB.races.push(newRace);
     saveToDatabase(DB);
     return newRace;
-  } catch (error) {
-    throw { status: 500, message: error?.message || error };
+    } catch (error) {
+      throw { status: 500, message: error?.message || error };
   }
 };
   
 const updateOneRace = (raceId, changes) => {
-  const indexForUpdate = DB.races.findIndex(
-    (race) => race.id === raceId
-  );
-  if (indexForUpdate === -1) {
-    return;
-  }
-  const updatedRace = {
-    ...DB.races[indexForUpdate],
-    ...changes,
-    updatedAt: new Date().toLocaleString("en-US", { timeZone: "UTC"}),
+  try {
+    const isAlreadyAdded =
+      DB.races.findIndex((race) => race.name === changes.name) > -1;
+    if (isAlreadyAdded) {
+      throw {
+        status: 400,
+        message: `Race with the name '${changes.name}' already exist`,
+      };
+    }
+    const indexForUpdate = DB.races.findIndex(
+      (race) => race.id === raceId
+    );
+    if (indexForUpdate === -1) {
+      throw {
+        status: 400,
+        message: `Can not find race with the ID '${raceId}'`,
+      };
+    }
+    const updatedRace = {
+      ...DB.races[indexForUpdate],
+      ...changes,
+      updatedAt: new Date().toLocaleString("en-US", { timeZone: "UTC"}),
+    };
+    DB.races[indexForUpdate] = updatedRace;
+    saveToDatabase(DB);
+    return updatedRace;
+    } catch (error) {
+    throw { status: error?.status || 500, message: error?.message || error };
+    }
   };
-  DB.races[indexForUpdate] = updatedRace;
-  saveToDatabase(DB);
-  return updatedRace;
-};
 
 const deleteOneRace = (raceId) => {
-  const indexForDeletion = DB.races.findIndex(
+  try {
+    const indexForDeletion = DB.races.findIndex(
     (race) => race.id === raceId 
-  );
-  if (indexForDeletion === -1) {
-    return;
-  }
-  DB.races.splice(indexForDeletion, 1);
-  saveToDatabase(DB);
-};
+    );
+    if (indexForDeletion === -1) {
+      throw {
+        status: 400,
+        message: `Can not find race with ID '${raceId}'`,
+      };
+    }
+    DB.races.splice(indexForDeletion, 1);
+    saveToDatabase(DB);
+    } catch (error) {
+      throw { status: error?.status || 500, message: error?.message || error };
+    }
+  };
 
 module.exports = { 
   getAllRaces,
